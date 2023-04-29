ce73e0cdd9363012b6d119dcb3f5af56070771f2
jhonmcc <jhonmcc@gmail.com>
2023-04-29 20:35:43 -0300

    Adicionado exemplo de classe o.o. Carro e sua utilizaçao

---
+++ Carro/AppCar.py
@@ -0,0 +1,8 @@
+from Carro import Carro
+
+
+# use example class car
+objCarChev = Carro("vectra", "Chevrolet", True)
+print(objCarChev)
+objCarChev.ligaDesliga(False)
+print(objCarChev)
---
+++ Carro/Carro.py
@@ -0,0 +1,36 @@
+class Carro:
+    # construtor initial class and initial vars class
+    def __init__(self, modelo='', marca='', ligado_desligado=False):
+        self.modelo = modelo
+        self.marca = marca
+        self.ligado_desligado = ligado_desligado
+
+    # encapsulate 
+    def setModelo(self, modelo):
+        self.modelo = modelo
+
+    def setmarca(self, marca):
+        self.marca = marca
+
+    def getModelo(self):
+        return self.modelo
+
+    def getmarca(self):
+        return self.marca
+    
+    def setLigadoDesligado(self, ligado_desligado):
+        self.ligado_desligado = ligado_desligado
+
+    def getLigadoDesligado(self):
+        return self.ligado_desligado
+
+    # methods class 
+    def ligaDesliga(self, ligado_desligado):
+        self.setLigadoDesligado(ligado_desligado)
+
+    
+    # print represent class example
+    def __repr__(self) -> str:
+        return self.modelo + " - " + str(self.ligado_desligado)
+
+
